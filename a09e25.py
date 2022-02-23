ffaeee788ed1b5319ad1fd05f0ec8ed4b6bef6b7
Higor Cirne <cirnne1@outlook.com>
2022-02-23 15:32:46 -0300

    edições no readme

--- a09e25.py
+++
@@ -1,2 +0,0 @@
-nome = str(input('Digite seu nome completo: ')).strip()
-print('SILVA' in nome.upper())
--- a09e27.py
+++
@@ -1,4 +0,0 @@
-nome = str(input('Digite seu nome completo: ')).strip()
-dividido = nome.split()
-print('Seu primeiro nome é {}'.format(dividido[0]))
-print('Seu último nome é {}'.format(dividido[-1]))

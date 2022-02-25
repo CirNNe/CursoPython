4c0721027bd7150e7532f41dfe1886b1e041a972
Higor Cirne <cirnne1@outlook.com>
2022-02-25 12:30:38 -0300

    últimos exercícios

--- exercícios/a10e35.py
+++ exercícios/a10e35.py
@@ -1,7 +1,7 @@
-r1 = int(input('Digite o valor da primeira reta: '))
-r2 = int(input('Digite o valor da segunda reta: '))
-r3 = int(input('Digite o valor da terceira reta: '))
-if r1 + r2 > r3 or r2 + r3 > r1 or r3 + r1 > r2:
+r1 = float(input('Digite o valor da primeira reta: '))
+r2 = float(input('Digite o valor da segunda reta: '))
+r3 = float(input('Digite o valor da terceira reta: '))
+if r1 < r2 + r3 and r2 < r3 + r1 and r3 < r1 + r2:
     print('Essas retas podem formar um triângulo!')
 else:
     print('O valor dessas retas são insuficientes para formar um triângulo!')
